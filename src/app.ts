0060b8dcc66090caabd036919dccf1be653775f3
dyoshyy <d.yoshhyy@gmail.com>
2024-05-24 08:25:21 +0900

    wip: zod validation

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,45 @@
 import { Hono } from 'hono'
 import { fibonacci } from './funcs/fib'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
 
 const app = new Hono()
 
-app.get('/fib', (c) => {
-  // Get the query parameter n
-  const n = parseFloat(c.req.query('n') || '')
+const schema = z.object({
+  n: z
+  .string()
+  .min(1, { message: 'Query parameter is required' })
+  .transform((val) => parseInt(val))
+  .refine((val) => !isNaN(val), { message: 'Not a number' })
+});
 
-  // Check if n is a number
-  if (isNaN(n)) {
-    return c.json({ error: 'n must be a number' }, 400)
+app.get('/', (c) => {
+  return c.text('usage: go to /fib?n=5 to calculate the 5th Fibonacci number')
   }
+)
 
-  // Check if n is an integer
-  if (!Number.isInteger(n)) {
-    return c.json({ error: 'n must be an integer' }, 400)
+app.get(
+  '/fib',
+  zValidator(
+    "query", 
+    schema,
+    (result, c) => {
+      if (!result.success) {
+        console.log(result.error.issues)
+        return c.json({ "status": result.error.issues[0].code, "message": result.error.issues[0].message}, 400)
+      }
+    }
+  ),
+  (c) => {
+
+    // バリデート済みの値を取得
+    const n = c.req.valid("query").n
+
+    // フィボナッチ数を計算
+    const result = fibonacci(n)
+
+    return c.json({ result })
   }
-
-  // Check if n is positive
-  if (n <= 0) {
-    return c.json({ error: 'n must be a positive integer' }, 400)
-  }
-
-  // Calculate the Fibonacci number
-  const result = fibonacci(n)
-
-  return c.json({ result })
-})
+)
 
 export default app
