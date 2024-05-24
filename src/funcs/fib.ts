5f762ff0bc8821d12ebf3b1929874145c2cc0f0e
dyoshyy <d.yoshhyy@gmail.com>
2024-05-24 23:40:38 +0900

    update: メモ化再帰の適用

--- src/funcs/fib.ts
+++ src/funcs/fib.ts
@@ -1,16 +1,16 @@
+const memo: { [key: number]: number } = {};
+
 export function fibonacci(n: number): number {
   if (n === 1 || n === 2) {
     return 1;
   }
 
-  let prev = 1;
-  let curr = 1;
-
-  for (let i = 3; i <= n; i++) {
-    const next = prev + curr;
-    prev = curr;
-    curr = next;
+  if (memo[n]) {
+    return memo[n];
   }
 
-  return curr;
+  const result = fibonacci(n - 1) + fibonacci(n - 2);
+  memo[n] = result;
+
+  return result;
 }
