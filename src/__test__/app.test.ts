61cfd139516ea7a9580ea7ce082884d3530bcfe7
dyoshyy <d.yoshhyy@gmail.com>
2024-05-25 10:03:22 +0900

    feat: CIの設定

--- src/__test__/app.test.ts
+++ src/__test__/app.test.ts
@@ -1,7 +1,6 @@
 import app from "../app"
 import { testCases } from "./app.cases";
 
-
 describe.each(testCases)('POST /', (testCase) => {
   it(testCase.description, async () => {
     const res = await app.request(`/fib?n=${testCase.queryParameter.n}`, {
